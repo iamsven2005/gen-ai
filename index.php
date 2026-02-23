8485b2e35d4dbb565bf11b88dddf7a924a5f3437
iamsven2005 <iamsven@gmail.com>
2026-02-23 16:20:27 +0800

    dd

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/includes/layout.php';
 require_once __DIR__ . '/includes/wizard.php';
 
 if (is_logged_in()) {
-    redirect('dashboard.php');
+    redirect('/gai/dashboard.php');
 }
 
 render_header('Welcome');
